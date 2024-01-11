09fbc63dc21821b8f7fac686d23f6c0a712fe7b7
vleenz <vincentleenz01@gmail.com>
2024-01-12 11:45:57 +1300

    added cv, removed contact

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+const navItems = document.querySelectorAll('nav li');
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+navItems.forEach(item => {
+    const link = item.querySelector('a');
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+    link.addEventListener('mouseover', function (e) {
+        link.style.textDecoration = 'underline';
+    });
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+    link.addEventListener('mouseout', function (e) {
+        link.style.textDecoration = '';
+    });
+});
---
+++ portfolio.js
@@ -0,0 +1,43 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+const navItems = document.querySelectorAll('nav li');
+
+navItems.forEach(item => {
+    const link = item.querySelector('a');
+
+    link.addEventListener('mouseover', function (e) {
+        link.style.textDecoration = 'underline';
+    });
+
+    link.addEventListener('mouseout', function (e) {
+        link.style.textDecoration = '';
+    });
+});
+
